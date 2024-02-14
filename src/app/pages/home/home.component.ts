991e8f883ed0705d3f534fa39d4aea37a00ab9f6
TomPrint <tomekklewicki@gmail.com>
2024-02-14 21:14:01 +0100

    add animations

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
 
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    AOS.init();
+  }
+
+  // Example method to demonstrate refreshing AOS
+  // Call this method if you dynamically load or change content
+  someContentLoadingMethod() {
+    // Assume this method loads or changes content dynamically
+    // ...
+
+    // After the content is loaded/changed
+    AOS.refresh();
+  }
 }
+
