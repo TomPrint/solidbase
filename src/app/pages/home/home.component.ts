ee4c8083d5458b36e5f1d3f54474f4f615a73438
TomPrint <tomekklewicki@gmail.com>
2024-05-27 22:32:43 +0200

    add text and button under carousel, add accordion with image, add partnerts section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -15,6 +15,28 @@ export class HomeComponent implements AfterViewInit {
     'assets/car5.jpg',
   ];
 
+  accordionItems = [
+    {
+      title: 'Jaki jest czas realizacji zamówienia i dostawy płyt betonowych?',
+      content: 'Nasz zespół logistyczny dokładnie planuje każde zlecenie, biorąc pod uwagę jego specyfikę i lokalizację dostawy, aby zapewnić optymalny czas realizacji.',
+      isOpen: true
+    },
+    {
+      title: 'Czy oferujecie usługi montażu płyt betonowych?',
+      content: 'Treść dotycząca usług montażu płyt betonowych.',
+      isOpen: false
+    },
+    {
+      title: 'Jakie zrównoważone praktyki są stosowane w procesie produkcji?',
+      content: 'Treść dotycząca zrównoważonych praktyk w procesie produkcji.',
+      isOpen: false
+    },
+    {
+      title: 'Czy możecie dostarczyć szczegółowe specyfikacje techniczne i dane dotyczące nośności płyt betonowych?',
+      content: 'Treść dotycząca specyfikacji technicznych i nośności płyt betonowych.',
+      isOpen: false
+    }
+  ];
 
 
 
@@ -50,7 +72,14 @@ export class HomeComponent implements AfterViewInit {
   previousImage() {
     this.currentIndex = (this.currentIndex + this.images.length - 1) % this.images.length;
   }
+
+
+  toggleAccordion(index: number) {
+    this.accordionItems[index].isOpen = !this.accordionItems[index].isOpen;
+  }
 }
+
+
 
 
 
